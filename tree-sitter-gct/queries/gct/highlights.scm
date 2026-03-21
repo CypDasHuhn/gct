3bcc7fc633b8cb8ad05ea91e52bf57286ca40743
cyp <martinfischer533@gmail.com>
2026-03-21 23:50:30 +0100

    color

---
+++ tree-sitter-gct/queries/gct/highlights.scm
@@ -0,0 +1,7 @@
+(tag_declaration (identifier) @type)
+(composite_declaration (identifier) @type)
+(attribute_declaration (identifier) @type)
+(tag_reference) @constant
+(attribute_reference) @variable
+(type) @keyword.type
+(value) @string
