2302d7602f45a38aa667733530b0ed8cb3107567
cyp <martinfischer533@gmail.com>
2026-03-22 11:25:41 +0100

    grammar update

--- tree-sitter-gct/grammar.js
+++ tree-sitter-gct/grammar.js
@@ -9,65 +9,41 @@
 module.exports = grammar({
   name: "gct",
   rules: {
-    source_file: $ => repeat1($.element),
-    element: $ => choice(
-      $.tag_declaration,
-      $.composite_declaration,
-      $.attribute_decleration
-    ),
-    identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    source_file: ($) => repeat1($.element),
+    element: ($) =>
+      choice(
+        $.tag_declaration,
+        $.composite_declaration,
+        $.attribute_decleration,
+      ),
+    identifier: (_) => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
-    tag_declaration: $ => seq(
-      "tag",
-      $.identifier
-    ),
-    composite_declaration: $ => seq(
-      "composite", 
-      $.identifier, 
-      "=", 
-     repeat1(choice($.tag_reference, $.attribute_reference)) 
-    ),
-    tag_reference: $ => /\#[a-zA-Z_][a-zA-Z0-9_]*/,
+    tag_declaration: ($) => seq("tag", $.identifier),
+    composite_declaration: ($) =>
+      seq(
+        "composite",
+        $.identifier,
+        "=",
+        repeat1(choice($.tag_reference, $.attribute_reference)),
+      ),
+    tag_reference: (_) => /\#[a-zA-Z_][a-zA-Z0-9_]*/,
 
     // region Attributes
-    attribute_decleration: $ => seq(
-      "attribute",
-      $.identifier,
-      $.type
-    ),
+    attribute_decleration: ($) => seq("attribute", $.identifier, $.type),
 
-    attribute_reference: $ => seq(
-      /\@[a-zA-Z_][a-zA-Z0-9_]*/,
-      "=",
-      $.value
-    ),
+    attribute_reference: ($) => seq(/\@[a-zA-Z_][a-zA-Z0-9_]*/, "=", $.value),
 
-    type: $ => choice(
-      "number",
-      "percentage",
-      "text",
-      "date",
-      "time",
-      "datetime"
-    ),
-    value: $ => choice(
-      $.datetime, 
-      $.date,
-      $.time,
-      $.percentage,
-      $.number,
-      $.text,
-    ),
+    type: (_) =>
+      choice("number", "percentage", "text", "date", "time", "datetime"),
+    value: ($) =>
+      choice($.datetime, $.date, $.time, $.percentage, $.number, $.text),
 
-    number: $ => /[0-9]+(\.[0-9]+)?/,
-    percentage: $ => /[0-9]+(\.[0-9]+)?%/,
-    text: $ => choice(
-      /[a-zA-Z0-9_]+/,
-      /"[^"]*"/
-    ),
-    date: $ => /[0-9]{4}-[0-9]{2}-[0-9]{2}/,
-    time: $ => /[0-9]{2}:[0-9]{2}(:[0-9]{2})?/,
-    datetime: $ => /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}(:[0-9]{2})?/,
+    number: (_) => /[0-9]+(\.[0-9]+)?/,
+    percentage: (_) => /[0-9]+(\.[0-9]+)?%/,
+    text: (_) => choice(/[a-zA-Z0-9_]+/, /"[^"]*"/),
+    date: (_) => /[0-9]{4}-[0-9]{2}-[0-9]{2}/,
+    time: (_) => /[0-9]{2}:[0-9]{2}(:[0-9]{2})?/,
+    datetime: (_) => /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}(:[0-9]{2})?/,
     // endregion
-  }
+  },
 });
