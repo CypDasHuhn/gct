e56fa870867af189381aea72ef271a0c26c9a1dd
cyp <martinfischer533@gmail.com>
2026-03-22 11:42:49 +0100

    colors

--- tree-sitter-gct/queries/gct/highlights.scm
+++ tree-sitter-gct/queries/gct/highlights.scm
@@ -1,7 +1,11 @@
+"tag" @keyword
+"composite" @keyword
+"attribute" @keyword
+
 (tag_declaration (identifier) @type)
 (composite_declaration (identifier) @type)
 (attribute_decleration (identifier) @type)
 (tag_reference) @constant
-(attribute_reference) @variable
-(type) @keyword.type
+(attribute_reference) @property
+(type) @type.builtin
 (value) @string
